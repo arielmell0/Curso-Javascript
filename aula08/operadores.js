a8469015adec9e797ab552a60cf4a331fb6ba297
Ariel <arielmello@rede.ulbra.br>
2020-08-15 22:54:20 -0300

    js

--- aula08/operadores.js
+++
@@ -1,7 +0,0 @@
-/*  Operadores relacionais 
-    5 > 2 -> true;
-    7 < 4 -> false;
-    8 >= 8 _> true;
-    9 <= 7 -> false;
-    5 == 5 -> true;
-    4 != 4 -> false; *\  
---
+++ aula08/operadores2.js
@@ -0,0 +1,15 @@
+/*  Operadores relacionais 
+
+    5 > 2 -> true;
+    7 < 4 -> false;
+    8 >= 8 _> true;
+    9 <= 7 -> false;
+    5 == 5 -> true;
+    4 != 4 -> false; 
+    
+    Identidade
+    
+    5 == 5 -> true;
+    5 == '5' -> true; 
+    5 === '5' -> false;
+    5 === 5 -> true;                *\
