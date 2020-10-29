a807382e39b406edaf7792fc21317edea2e0b476
Ariel <arielmello@rede.ulbra.br>
2020-10-29 01:38:38 -0300

    projetinhos

--- Javascript em 4 horas/Mini_projeto_2.js
+++ Javascript em 4 horas/Mini_projeto_2.js
@@ -1,5 +1,11 @@
-const listaVerduras = ['Morango', 'Nabo', 'Cenoura', 'Repolho', 'Alface', 'Beterraba']
+//Escreva uma função que escreva dois números e retorna o maior entre eles
 
-for(verdura of listaVerduras){
-    console.log(verdura)
+function maiorNumero(n1, n2) {
+    if (n1 > n2) {
+        return n1
+    } else {
+        return n2
+    }
 }
+
+console.log(maiorNumero(20, 15))
