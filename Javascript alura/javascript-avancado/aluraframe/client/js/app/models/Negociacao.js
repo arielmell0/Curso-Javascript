8af5f8d8a0a343000312244caf1af7669eb8fe9e
arielmell0 <arielmello@rede.ulbra.br>
2021-04-12 07:51:52 -0300

    feat: :sparkles: add a object freeze to the constructor

--- Javascript alura/javascript-avancado/aluraframe/client/js/app/models/Negociacao.js
+++ Javascript alura/javascript-avancado/aluraframe/client/js/app/models/Negociacao.js
@@ -3,6 +3,8 @@ class Negociacao{
         this._data = new Date(data.getTime())
         this._quantidade = quantidade
         this._valor = valor
+
+        Object.freeze(this)
     }
     
     obterVolume() {
