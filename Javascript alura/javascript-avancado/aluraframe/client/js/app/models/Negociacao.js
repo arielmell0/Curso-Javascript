cdc7381168a6c3ce234424a2c633ab6febbf1f73
arielmell0 <arielmello@rede.ulbra.br>
2021-04-11 22:17:12 -0300

    refactor: :pencil2: get sintax

--- Javascript alura/javascript-avancado/aluraframe/client/js/app/models/Negociacao.js
+++ Javascript alura/javascript-avancado/aluraframe/client/js/app/models/Negociacao.js
@@ -9,15 +9,15 @@ class Negociacao{
         return this._quantidade * this._valor
     }
 
-    getData(){
+    get data(){
         return this._data
     }
 
-    getQuantidade(){
+    get quantidade(){
         return this._quantidade        
     }
 
-    getValor(){
+    get valor(){
         return this._valor
     }
 
