f6ef2c6fecfe0d06a74a5ea9f1770f67399efabd
arielmell0 <arielmello@rede.ulbra.br>
2021-07-22 01:49:47 -0300

    feat: :sparkles: add elements objects function finished
    insertElements function add elements received from an array of objects and add to the document

--- javascript e typescript/mod_3_for_exercise/assets/js/main.js
+++ javascript e typescript/mod_3_for_exercise/assets/js/main.js
@@ -1,7 +1,18 @@
 const elements = [
-    { tag: 'p', texto: 'Frase 1'},
-    { tag: 'div', texto: 'Frase 2'},
-    { tag: 'footer', texto: 'Frase 3'},
-    { tag: 'section', texto: 'Frase 4'},
+    { tag: 'p', text: 'Frase 1'},
+    { tag: 'div', text: 'Frase 2'},
+    { tag: 'footer', text: 'Frase 3'},
+    { tag: 'section', text: 'Frase 4'},
 ]
 
+function insertElements(){
+    const mainSection = document.querySelector("#main-section");
+
+    for (let i = 0; i < elements.length; i++) {
+        const {tag, text} = elements[i]
+
+        mainSection.innerHTML += `<${tag}>${text}</${tag}`
+    }
+}
+
+insertElements()
