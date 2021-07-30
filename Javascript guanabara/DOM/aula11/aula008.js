5b4a08668625f30970498056db5d50e54385c8b0
arielmell0 <arielmello@rede.ulbra.br>
2021-07-30 00:30:43 -0300

    refactor: :fire: deleted files
    deleted the olf files of the old folder structure

--- Javascript guanabara/DOM/aula11/aula008.js
+++
@@ -1,7 +0,0 @@
-var vel = 54
-console.log(`A velocidade do seu carro é ${vel}Km/h`)
-if(vel > 60){
-    console.log('Você está acima da velocidade permitida!!!')
-} else {
-    console.log('Tudo ok por aqui, nenhuma multa foi registrada.')
-}
