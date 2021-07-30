2deeb808a335f6e8ce45e137a39261427d110dd0
arielmell0 <arielmello@rede.ulbra.br>
2021-07-30 00:35:45 -0300

    refactor: :art: added each course to a branch

--- Javascript em 4 horas/básico/for-off.js
+++
@@ -1,9 +0,0 @@
-//for-off 
-//mais fácil de trabalhar e introduzido no ecs6
-//não precisa de indices 
-
-const cores = ['Vermelho', 'Azul', 'Verde']
-
-for(let cor of cores){
-    console.log(cor)
-}
